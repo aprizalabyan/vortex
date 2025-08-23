debb3eca637cee7ff9fdedfa793b3a968578ead3
aprizalabyan <abi@jkt1.ebdesk.com>
2025-08-23 23:47:15 +0700

    feat: setup svg as react-component for custom icon

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {
+    rules: {
+      // import MyIcon from './icon.svg' -> React component
+      "*.svg": {
+        loaders: ["@svgr/webpack"],
+        as: "*.js",
+      },
+    },
+  },
 };
 
 export default nextConfig;
